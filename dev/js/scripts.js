9325528065d70e1978139f80e62fe5862fe59d96
MaddyLobb <81719007+MaddyLobb@users.noreply.github.com>
2022-02-28 21:37:54 -0500

    light/dark mode

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,24 +1,30 @@
-// import { gsap } from "gsap";
-// import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-// gsap.registerPlugin(DrawSVGPlugin);
+function switchTheme(e){
 
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme',light);
+    }
 
-
-// // gsap.set("#path",{transformOrigin: "50% 50%", xPercent: -50, yPercent: -50});
-// // const duration = 0.75;
-
-// const mainTL = gsap.timeline();
-
-
-// // name of the timeline | do you want to start at the end or the beginning of the timeline?  | What do you want to animate? | { how long is the animation? | what do you want to do?}
-
-// // pathOne or circle starts at begining of timeline and will draw out fully then disapear
-// mainTL.to("#pathOne",{duration: 2, drawSVG:"0%"});
+    else{
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme',dark); 
+    }
+    
+    console.log(toggleSwitch.checked +'this is the value for the checkbox');
 
 
-// // hero text will starts at end of the timeline and will fade onto screen -1 second before the end of the previous animation of a circle and last 2 seconds
-// mainTL.from("#text",{duration:3, alpha: 0},">-1");
+}
 
-// // header or navbar text starts at end of the timeline and will fade onto screen -2 seconds before the end of the previous animation of hero text and will last .5 seconds 
-// mainTL.from("#header", {duration: .5, alpha: 0},">-2");
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') :null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'light'){
+        toggleSwitch.checked = true;
+    }
+}
